23a91c5d77526b3fad3f83e6b20fbc18711a4441
Hyeonhye <126955136+hyeonhye126@users.noreply.github.com>
2024-10-27 17:17:22 +0900

    [level 0] Title: 조건 문자열, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181934. 조건 문자열/조건 문자열.py
@@ -0,0 +1,23 @@
+def solution(ineq, eq, n, m):
+    if ineq == '>' and eq == '=':
+        if n >= m:
+            answer = 1
+        else:
+            answer = 0
+    elif ineq == '<' and eq == '=':
+        if n <= m:
+            answer = 1
+        else:
+            answer = 0
+    elif ineq == '>' and eq == '!':
+        if n > m:
+            answer = 1
+        else:
+            answer = 0
+    else:
+        if n < m:
+            answer = 1
+        else:
+            answer = 0
+        
+    return answer
