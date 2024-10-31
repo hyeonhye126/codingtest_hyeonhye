13e9a128e0a476c0b1fbb116a1a2e6db48ea42a8
Hyeonhye <126955136+hyeonhye126@users.noreply.github.com>
2024-10-31 09:52:48 +0900

    [level 0] Title: 수열과 구간 쿼리 3, Time: 0.19 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.py
+++ 프로그래머스/0/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.py
@@ -1,13 +1,6 @@
 def solution(arr, queries):
-    answer = []
-    
     for query in queries:
         i = query[0]
         j = query[1]
-
-        tmp = arr[i]
-        arr[i] = arr[j]
-        arr[j] = tmp
-        
-    answer = arr
-    return answer
+        arr[i], arr[j] = arr[j], arr[i]
+    return arr
