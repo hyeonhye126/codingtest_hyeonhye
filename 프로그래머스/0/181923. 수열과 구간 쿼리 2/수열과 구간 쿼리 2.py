bc6bee0d522b0716f937d7b57efc0d35774ee47f
Hyeonhye <126955136+hyeonhye126@users.noreply.github.com>
2024-11-01 13:41:59 +0900

    [level 0] Title: 수열과 구간 쿼리 2, Time: 18.69 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.py
+++ 프로그래머스/0/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.py
@@ -2,15 +2,15 @@ def solution(arr, queries):
     answer = []
     
     for s, e, k in queries:
-        min_ = 1000000
+        tmp = []
         for i in range(s, e + 1):
             if arr[i] > k:
-                if min_ > arr[i]:
-                    min_ = arr[i]
+                tmp.append(arr[i])
     
-        if min_ == 1000000:
+        if not tmp:
             answer.append(-1)
         else:
-            answer.append(min_)
+            result = min(tmp)
+            answer.append(result)
                 
     return answer
