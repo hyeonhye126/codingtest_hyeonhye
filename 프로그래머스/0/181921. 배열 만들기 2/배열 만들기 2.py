1447185103165c437059356f68d9084a864acf23
Hyeonhye <126955136+hyeonhye126@users.noreply.github.com>
2024-11-04 18:11:53 +0900

    [level 0] Title: 배열 만들기 2, Time: 686.52 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.py
+++ 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.py
@@ -1,13 +1,6 @@
 def solution(l, r):
     answer = []
-    for i in range(l, r + 1):
-        flag = 0
-        if i % 5 == 0:
-            for c in str(i):
-                if c != '5' and c != '0':
-                    flag = 1
-            if flag == 0:
-                answer.append(i)         
-    if not answer:
-        answer.append(-1)
-    return answer
+    for num in range(l, r + 1):
+        if not (set(str(num)) - set(['0', '5'])):
+            answer.append(num)  
+    return answer if answer else [-1]
