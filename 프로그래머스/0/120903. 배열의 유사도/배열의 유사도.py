052b5bdf02a0cadfe5dd7ee2a9621339f86ba292
Hyeonhye <126955136+hyeonhye126@users.noreply.github.com>
2024-12-11 15:02:43 +0900

    [level 0] Title: 배열의 유사도, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.py
+++ 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.py
@@ -1,13 +1,7 @@
 def solution(s1, s2):
     cnt = 0
-    if len(s1) > len(s2):
-        for i in s1:
-            for j in s2:
-                if i == j:
-                    cnt += 1
-    else:
-        for i in s2:
-            for j in s1:
-                if i == j:
-                    cnt += 1
+    for i in s1:
+        for j in s2:
+            if i == j:
+                cnt += 1
     return cnt
