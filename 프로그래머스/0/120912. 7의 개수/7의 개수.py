f9762705eb3f3f9946c2644db00638a62f2dc9c0
Hyeonhye <126955136+hyeonhye126@users.noreply.github.com>
2024-12-16 03:05:49 +0900

    [level 0] Title: 7의 개수, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/120912. 7의 개수/7의 개수.py
+++ 프로그래머스/0/120912. 7의 개수/7의 개수.py
@@ -1,7 +1,2 @@
 def solution(array):
-    cnt = 0
-    for ch in str(array):
-        for c in ch:
-            if c == '7':
-                cnt += 1
-    return cnt
+    return str(array).count('7')
