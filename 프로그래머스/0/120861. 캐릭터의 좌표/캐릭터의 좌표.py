77b7e79291c87c3fd3e049adc354f6c88cd304ad
Hyeonhye <126955136+hyeonhye126@users.noreply.github.com>
2025-01-07 19:24:28 +0900

    [level 0] Title: 캐릭터의 좌표, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120861. 캐릭터의 좌표/캐릭터의 좌표.py
+++ 프로그래머스/0/120861. 캐릭터의 좌표/캐릭터의 좌표.py
@@ -1,21 +1,21 @@
 def solution(keyinput, board):
-    start = [0, 0]
+    position = [0, 0]
+    
+    max_x = board[0] // 2
+    max_y = board[1] // 2
     
     for k in keyinput:
-        # if abs(start[0]) == board[0] // 2 or abs(start[1]) == board[1] // 2:
-        #     continue
-        
         if k == "left":
-            if start[0] != -(board[0] // 2):
-                start[0] -= 1
+            if position[0] > -max_x:
+                position[0] -= 1
         elif k == "right":
-            if start[0] != board[0] // 2:
-                start[0] += 1
+            if position[0] < max_x:
+                position[0] += 1
         elif k == "down":
-            if start[1] != -(board[1] // 2):
-                start[1] -= 1
+            if position[1] > -max_y:
+                position[1] -= 1
         else:
-            if start[1] != board[1] // 2:
-                start[1] += 1
+            if position[1] < max_y:
+                position[1] += 1
     
-    return start
+    return position
