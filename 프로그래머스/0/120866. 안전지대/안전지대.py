7655aa5640217843cdb1df8299926b8c981e78b2
Hyeonhye <126955136+hyeonhye126@users.noreply.github.com>
2025-01-12 23:37:18 +0900

    [level 0] Title: 안전지대, Time: 0.10 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120866. 안전지대/안전지대.py
+++ 프로그래머스/0/120866. 안전지대/안전지대.py
@@ -1,23 +1,3 @@
 def solution(board):
-    n = len(board)
-    
-    danger_zone = [[0] * n for _ in range(n)]
-    
-    directions = [(-1, 0), (1, 0), (0, -1), (0, 1), (-1, -1), (-1, 1), (1, -1), (1, 1)]
-    
-    for r in range(n):
-        for c in range(n):
-            if board[r][c] == 1:
-                danger_zone[r][c] = 1
-                for dr, dc in directions:
-                    nr, nc = r + dr, c + dc
-                    if 0 <= nr < n and 0 <= nc < n:
-                        danger_zone[nr][nc] = 1
-    
-    safe_count = 0
-    for r in range(n):
-        for c in range(n):
-            if board[r][c] == 0 and danger_zone[r][c] == 0:
-                safe_count += 1
-
-    return safe_count
+    answer = 0
+    return answer
