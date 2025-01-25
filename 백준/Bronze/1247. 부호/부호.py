5e3eb7ab1db40a24af85d7f347c730f6ea30b96e
Hyeonhye <126955136+hyeonhye126@users.noreply.github.com>
2025-01-26 00:30:36 +0900

    [Bronze III] Title: 부호, Time: 136 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/1247. 부호/부호.py
@@ -0,0 +1,13 @@
+import sys
+input = sys.stdin.readline
+
+arr = []
+for _ in range(3):
+    N = int(input())
+    flag = sum(int(input()) for _ in range(N))
+    if flag == 0:
+        print(0)
+    elif flag < 0:
+        print('-')
+    else:
+        print('+')
