b40d50307d88884f8ff2adc3f218796e36174e8e
Hyeonhye <126955136+hyeonhye126@users.noreply.github.com>
2025-07-27 00:05:16 +0900

    Delete 프로그래머스/1/68644. 두 개 뽑아서 더하기 directory

--- 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.py
+++
@@ -1,9 +0,0 @@
-def solution(numbers):
-    sum = []
-    for i in range(len(numbers)):
-        for j in range(len(numbers)):
-            if i != j:
-                sum.append(numbers[i] + numbers[j])
-    sum = sorted(list(set(sum)))
-    answer = sum
-    return answer
