75fc67df00223138a367676426249db27b866794
Hyeonhye <126955136+hyeonhye126@users.noreply.github.com>
2025-08-10 00:52:05 +0900

    [level 0] Title: 다음에 올 숫자, Time: 0.00 ms, Memory: 9.29 MB -BaekjoonHub

---
+++ 프로그래머스/0/120924. 다음에 올 숫자/다음에 올 숫자.py
@@ -0,0 +1,11 @@
+from collections import Counter
+
+def solution(common):
+    answer = 0
+    
+    if common[1] - common[0] == common[2] - common[1]:
+        answer = common[-1] + (common[1] - common[0])
+    elif common[1] // common[0] == common[2] // common[1]:
+        answer = common[-1] * (common[1] // common[0])
+    
+    return answer
