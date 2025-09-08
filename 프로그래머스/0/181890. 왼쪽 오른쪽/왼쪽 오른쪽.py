6ff74065672e5bea7e7e7af16832a2097af6c225
Hyeonhye <126955136+hyeonhye126@users.noreply.github.com>
2025-09-08 23:44:15 +0900

    [level 0] Title: 왼쪽 오른쪽, Time: 0.00 ms, Memory: 9.07 MB -BaekjoonHub

--- 프로그래머스/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.py
+++ 프로그래머스/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.py
@@ -1,12 +1,3 @@
 def solution(str_list):
     answer = []
-    
-    for i in range(len(str_list)):
-        if str_list[i] == 'l':
-            answer = str_list[:i]
-            break;
-        if str_list[i] == 'r':
-            answer = str_list[i+1:]
-            break
-            
     return answer
