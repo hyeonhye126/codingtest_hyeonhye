5c41dae9932b24be0889ff21e6c7f7e49154ae76
Hyeonhye <126955136+hyeonhye126@users.noreply.github.com>
2025-11-05 10:30:40 +0900

    [Silver III] Title: 도키도키 간식드리미, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/12789. 도키도키 간식드리미/도키도키 간식드리미.py
@@ -0,0 +1,21 @@
+N = int(input())
+num = list(map(int, input().split()))
+
+stack = []
+need = 1
+
+for x in num:
+    while stack and stack[-1] == need:
+        stack.pop()
+        need += 1
+
+    if x == need:
+        need += 1
+    else:
+        stack.append(x)
+
+while stack and stack[-1] == need:
+        stack.pop()
+        need += 1
+
+print("Sad") if stack else print("Nice")
