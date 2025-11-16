5d69fdcc1ef1eff9042ceb9d24d4db685f99c15b
Hyeonhye <126955136+hyeonhye126@users.noreply.github.com>
2025-11-16 19:41:26 +0900

    [level 0] Title: 특별한 이차원 배열 2, Time: 0.89 ms, Memory: 9.54 MB -BaekjoonHub

---
+++ 프로그래머스/0/181831. 특별한 이차원 배열 2/특별한 이차원 배열 2.py
@@ -0,0 +1,11 @@
+def solution(arr):
+    flag = 0
+    
+    for i in range(len(arr)):
+        for j in range(len(arr[i])):
+            if arr[i][j] == arr[j][i]:
+                flag = 1
+            else:
+                return 0
+    
+    return flag
