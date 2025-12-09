decc2c56d9bca22a2b9615d2618e87c65a8a098b
Hyeonhye <126955136+hyeonhye126@users.noreply.github.com>
2025-12-09 09:42:14 +0900

    [Silver IV] Title: 가로수, Time: 2480 ms, Memory: 44056 KB -BaekjoonHub

---
+++ 백준/Silver/2485. 가로수/가로수.py
@@ -0,0 +1,16 @@
+import math
+
+N = int(input())
+
+x = []
+for i in range(N):
+    x.append(int(input()))
+
+diffs = [x[i+1] - x[i] for i in range(N-1)]
+
+g = diffs[0]
+for d in diffs[1:]:
+    g = math.gcd(g,d)
+
+total_tree = (x[-1] - x[0]) // g  + 1
+print(total_tree - N)
